5a33f968cd96fbc868ba51a0c4934e38d15fbbcc
jimjamz <jimjamz@outlook.com>
2020-11-11 21:30:23 +0000

    'Discovering BDD' and 'Your First Scenario'

---
+++ shouty/lib/shouty.rb
@@ -0,0 +1,13 @@
+module Shouty
+    class Person
+        def move_to(distance)
+
+        end
+        def shout(message)
+
+        end
+        def messages_heard
+            ["Hello!", "Free bagels at Sean's"]
+        end
+    end
+end
